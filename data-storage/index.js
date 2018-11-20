3a352d5c14d335feb9b6d99817a9462f517e5c26
ThiagoFazzi <thiago@xtwo.com.br>
2018-11-20 15:10:36 +0100

    set up mongodb connection

--- data-storage/index.js
+++ data-storage/index.js
@@ -1,16 +1,37 @@
-'use strict';
-var nats = require('nats').connect();
+'use strict'
+const nats = require('nats').connect()
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/vehicle');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('mongoDB connected!')
+
+    
+
+
+
+
+
+
+});
+
+
+
+
 
 //check for NATS error
 nats.on('error', function(e) {
-    console.log('Error [' + nats.options.url + ']: ' + e);
-    process.exit();
-});
+    console.log('Error [' + nats.options.url + ']: ' + e)
+    process.exit()
+})
 
 //Define NATS subject to looking for
-var subject = 'vehicle.test-bus-1';
+var subject = 'vehicle.test-bus-1'
 
 //subscribe with nats for a especific subject, in this case 'vehicle.test-bus-1' to receive data
 nats.subscribe(subject, function(msg) {
     console.log(msg);
-});
+})
