1c4622f2f7ac4389e87134ef7630fab1db3fd90f
ThiagoFazzi <thiago@xtwo.com.br>
2018-11-22 23:07:54 +0100

    setup initial component maps

--- client/src/components.js/map-component.js
+++ client/src/components.js/map-component.js
@@ -1,15 +1,30 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-class MapComponent extends Component {
 
-  render(){
-    return(
-      <div>
-        <h1>Map</h1>
-      </div>
-    )
-    
+const mapStyles = {
+  width: '50%',
+  height: '100%',
+  float: 'right',
+  position: 'absolute'
+};
+
+export class MapComponent extends Component {
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={16}
+        style={mapStyles}
+        initialCenter={{
+         lat: -1.2884,
+         lng: 36.8233
+        }}
+      />
+    );
   }
 }
 
-export default MapComponent
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyA1hygOKnk5N5aJ3-oHHzYQ-031Ug-OP_U'
+})(MapComponent);
