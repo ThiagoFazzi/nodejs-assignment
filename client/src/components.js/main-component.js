220c5a3aa1291ac3c3b5324a0b014485760c794d
ThiagoFazzi <thiago@xtwo.com.br>
2018-11-23 12:06:26 +0100

    add component map and vehicle runs over the map

--- client/src/components.js/main-component.js
+++ client/src/components.js/main-component.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react'
-import MapComponent from './map-component';
 import GaugeComponent from './gauge-component';
 import socketIOClient from "socket.io-client";
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+
+const styles = {
+  map:{
+    width: '60%',
+    height: '100%',
+    //float: 'right',
+  },
+  chart: {
+    top: '100px',
+    right: '100px',
+    position: 'absolute',
+  }
+  //position: 'absolute'
+};
 
 class MainComponent extends Component {
 
   constructor(){
     super()
-    this.state = {speed: 0};
+    this.state = {
+      speed: 0,
+      lat: 52.093448638916016,
+      lng: 5.117378234863281,
+    };
   }
 
 
@@ -15,7 +33,13 @@ class MainComponent extends Component {
     //const { endpoint } = this.state;
     const socket = socketIOClient('ws://localhost:8080');
     //socket.on("FromAPI", data => this.setState({ response: data }));
-    socket.on("Vehicle", data => this.setState({speed: data.speed}))
+    socket.on("Vehicle", data => {
+      this.setState({
+        speed: data.speed,
+        lat: data.gps[0],
+        lng: data.gps[1]
+      })
+    })
   }
 
 
@@ -23,17 +47,36 @@ class MainComponent extends Component {
   render() {
     return (
       <div>
+        <div style={styles.chart}>
         <GaugeComponent
           color="green"
           strokeWidth="5"
           sqSize="100"
           percentage={this.state.speed}
         />
-        <MapComponent />
+        </div>
+        <Map
+          google={this.props.google}
+          zoom={16}
+          style={styles.map}
+          center={{
+            lat: `${this.state.lat}`,
+            lng: `${this.state.lng}`
+          }}
+        >
+          <Marker
+            name={'Your position'}
+            //position={{lat: 52.093448638916016, lng:  5.117378234863281}}
+            position={{lat: `${this.state.lat}`, lng:  `${this.state.lng}`}}
+          />
+        </Map>
+        
       </div>
     )
   }
 }
 
-export default MainComponent
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyA1hygOKnk5N5aJ3-oHHzYQ-031Ug-OP_U'
+})(MainComponent);
 
--- client/src/components.js/map-component.js
+++ client/src/components.js/map-component.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-
 const mapStyles = {
   width: '50%',
   height: '100%',
@@ -11,16 +10,23 @@ const mapStyles = {
 
 export class MapComponent extends Component {
   render() {
+
     return (
       <Map
         google={this.props.google}
         zoom={16}
         style={mapStyles}
         initialCenter={{
-         lat: -1.2884,
-         lng: 36.8233
+         lat: 52.093448638916016,
+         lng: 5.117378234863281
         }}
+      >
+      <Marker
+        name={'Your position'}
+        position={{lat: {}, lng: -122.439192}}
       />
+      </Map>
+      
     );
   }
 }
